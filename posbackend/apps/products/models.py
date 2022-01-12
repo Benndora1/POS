4dd770431556a5ba48dd5d7ec8a4d0f3a6b2b398
benndora1 <benndora2@gmail.com>
2022-01-12 21:29:07 +0300

    products

--- posbackend/apps/products/models.py
+++ posbackend/apps/products/models.py
@@ -1,5 +1,10 @@
+from typing import Callable
 from django.db import models
 from django.db.models.aggregates import Max
+from django.db.models.deletion import CASCADE
+from apps.attribute.models import Attribute
+from apps.brands.models import Brands
+from apps.category.models import Category
 
 # Create your models here.
 class Products(models.Model):
@@ -10,7 +15,8 @@ class Products(models.Model):
     image_url = models.CharField(max_length=255, null=True, blank=True)
     description = models.TextField(blank=True,null=True)
     availabilty = models.CharField(max_length=100)
-    # attribute_value_id
-    # brand_id
-    # categor_id
+    attribute_id = models.ForeignKey(Attribute, on_delete=CASCADE)
+    brand_id = models.ForeignKey(Brands, on_delete=CASCADE)
+    category_id = models.ForeignKey(Category, on_delete=CASCADE)
+
     # store_id
